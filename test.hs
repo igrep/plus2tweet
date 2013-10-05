839fd713decab380244d7815f20b44bca6e4d69f
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-10-06 00:26:51 +0900

    add command to try to parse the content of a post. but HXT doesn't accept.

--- test.hs
+++ test.hs
@@ -3,13 +3,15 @@ module Main where
 import GooglePlus.Activity
 import Settings
 
-import Data.Aeson ( eitherDecode )
+import Data.Aeson ( eitherDecode, FromJSON )
+import qualified Text.XML.HXT.Core as HXT
 
 import qualified Data.ByteString.Lazy as BSL
 import System.Environment ( getArgs )
 import Control.Applicative ( (<$>) )
 import qualified Data.Text.IO as TIO
 import Data.Text ( Text )
+import Data.String ( fromString )
 
 main :: IO ()
 main = do
@@ -17,7 +19,8 @@ main = do
   exec cmd args
   where
     exec "dl" = tryDownload
-    exec "parse" = tryParseJSON
+    exec "parse-j" = tryParseJSON
+    exec "parse-c" = tryParseContent
     exec "e-content" = tryExtractContentWith TIO.putStrLn
     exec "e-content-show" = tryExtractContentWith print
     exec other = error $ "Undifiend command: " ++ other
@@ -31,12 +34,23 @@ tryParseJSON :: [FilePath] -> IO ()
 tryParseJSON args =
   print =<< loadActivitiesList ( head args )
 
+tryParseContent :: [String] -> IO ()
+tryParseContent args = do
+  decoded <- return $ decodeString $ head args
+  putStrLn decoded
+  print =<< ( HXT.runX $ HXT.readString [HXT.withParseHTML HXT.yes] $ "<html><body>" ++ decoded ++ "</body></html>" )
+  where
+    decodeString = unsafeDecode . fromString
+
 tryExtractContentWith :: ( Text -> IO() ) -> [FilePath] -> IO ()
 tryExtractContentWith printer args =
   mapM_ ( printer . content . activityObject ) =<< items <$> loadActivitiesList ( head args )
 
 loadActivitiesList :: FilePath -> IO ActivitiesList
 loadActivitiesList jsonPath = unsafeDecode <$> BSL.readFile jsonPath
-  where
-    unsafeDecode = leftError . eitherDecode
-    leftError = either error id
+
+unsafeDecode :: FromJSON a => BSL.ByteString -> a
+unsafeDecode = leftError . eitherDecode
+
+leftError :: Either String a -> a
+leftError = either error id
