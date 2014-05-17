127db6fedb5e7865b1920a0bbe6e15434deab045
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2014-05-17 14:41:57 +0900

    split dependency and compile Spec.hs

--- test/GooglePlus/ActivitySpec.hs
+++ test/GooglePlus/ActivitySpec.hs
@@ -11,8 +11,8 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck ( (==>) )
 
-main :: IO ()
-main = hspec $ do
+spec :: Spec
+spec = do
   describe "convertToOriginalContent" $ do
     prop "convert <b> to aseterisk" $ \ s1 s2 s3 ->
       hasNoLt s1 && hasNoLt s2 && hasNoLt s3 ==>
